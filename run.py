c5ad022c617a21e88afaf0e96584cfee3415f5e3
Ed Whyatt <lizardxyz@googlemail.com>
2022-09-10 15:26:48 +0000

    Add if statment to tell the app how to run the application

--- run.py
+++ run.py
@@ -1,2 +1,10 @@
 import os
 from movielounge import app
+
+# Tell the app where and how to run the application 
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG")
+    )
