d075a2c1c24dfb84e53ade0940afa3745978e5e0
Ed Whyatt <lizardxyz@googlemail.com>
2022-09-10 15:17:08 +0000

    Add basic app route for base.html template

--- movielounge/routes.py
+++ movielounge/routes.py
@@ -1,2 +1,7 @@
 from flask import flash, render_template, request, redirect, session, url_for
 from movielounge import app, db
+
+
+@app.route("/")
+def home():
+    return render_template("base.html")
