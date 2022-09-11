476b1cbd91e3c4cb8d1bca81cd48b7144ad50b78
Ed Whyatt <lizardxyz@googlemail.com>
2022-09-11 19:49:37 +0000

    Add register post request to add user to user database

--- movielounge/routes.py
+++ movielounge/routes.py
@@ -14,9 +14,31 @@ def home():
 
 
 # --- Register page ---#
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     """
-    Route for register.html and register form post
+    Gets register.html register form to Post, then add details to the user db
     """
+    if request.method == "POST":
+        # Check if username already exists in db
+        existing_user = Users.query.filter(
+            Users.user_name == request.form.get("username").lower()).all()
+
+        if existing_user:
+            flash("Usernmae already exists")
+            return redirect(url_for("register"))
+
+        user = Users(
+            user_name=request.form.get("username").lower(),
+            password=generate_password_hash(request.form.get("password"))
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
