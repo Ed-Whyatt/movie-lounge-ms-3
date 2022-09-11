d51eb6488f09398feed1ce519e2c5819c0a42872
Ed Whyatt <lizardxyz@googlemail.com>
2022-09-11 17:46:35 +0000

    Add import for werkzeug.security for log in and register

--- movielounge/routes.py
+++ movielounge/routes.py
@@ -1,4 +1,5 @@
 from flask import flash, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 from movielounge import app, db
 from movielounge.models import Category, Users
 
