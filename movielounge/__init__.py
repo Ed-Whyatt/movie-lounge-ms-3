fd850eb08845681f0d8daa64bfc0d2f886ea218a
Ed Whyatt <lizardxyz@googlemail.com>
2022-09-22 18:08:50 +0000

    Update __init__.py file for heroku

--- movielounge/__init__.py
+++ movielounge/__init__.py
@@ -10,11 +10,12 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["API_KEY"] = os.environ.get("API_KEY")
 
+if os.environ.get("DEVelopment") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 mongo = PyMongo(app)
