f622d0841364729238dd551eb06ba144b6541a14
Ed Whyatt <lizardxyz@googlemail.com>
2022-10-03 18:10:55 +0000

    Fix space in comment for pep8 testing

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import os
 from movielounge import app
 
 
-# Tell the app where and how to run the application 
+# Tell the app where and how to run the application
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
